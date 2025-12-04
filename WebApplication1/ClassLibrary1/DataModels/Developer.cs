0eb97befdb93a8885946b648cad98cea4a6f7ff9
AntonBessarab <144679203+AntonBessarab@users.noreply.github.com>
2025-12-04 22:24:06 +0200

    feat: added validation to developers

--- WebApplication1/ClassLibrary1/DataModels/Developer.cs
+++ WebApplication1/ClassLibrary1/DataModels/Developer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,18 @@ namespace ClassLibrary1.DataModels
     public class Developer
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [StringLength(70)]
         public string Description { get; set; }
+        [EmailAddress]
+        [Required]
         public string ContactEmail { get; set; }
-        public ICollection<Game> Games { get; } = new List<Game>(); 
+        [NotMapped]
+        [Compare("ContactEmail")]
+        [Required]
+        public string ConfirmEmail { get; set; }
+        public ICollection<Game> Games { get; } = new List<Game>();
 
     }
 }
